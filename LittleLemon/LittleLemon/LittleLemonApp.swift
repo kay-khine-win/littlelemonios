25f4296a364e194b8e2dfc210142cf51f394383f
kaykhinewin <kkw19.win@gmail.com>
2023-05-16 09:23:34 +0630

    new project

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Kay Khine win on 16/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
